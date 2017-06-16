d258a6d290894b4184b21b8bf30efecc5b420754
Jerry Heiselman <jerry@heiselman.com>
2017-06-16 09:28:28 -0500

    Added check for XBOX_LIVE_ID environment variable or CLI option

--- cmd/xbone_on/main.go
+++ cmd/xbone_on/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/jheiselman/xbone"
 )
 
 func main() {
 	var xboxIP = flag.String("hostname", "XboxOne", "Hostname or IP address of Xbox")
-	var xboxLiveID = flag.String("liveid", "FD00A44B102D3341", "Xbox Live ID")
+	var xboxLiveID = flag.String("liveid", "", "Xbox Live ID")
 	flag.Parse()
+
+	if *xboxLiveID == "" && os.Getenv("XBOX_LIVE_ID") != "" {
+		*xboxLiveID = os.Getenv("XBOX_LIVE_ID")
+	}
+
+	if *xboxLiveID == "" {
+		fmt.Printf("You must pass -liveid option or set XBOX_LIVE_ID environment variable")
+		return
+	}
 
 	xbox := &xbone.Xbox{IP: *xboxIP, LiveID: *xboxLiveID, Debug: true}
 	if err := xbox.TurnOn(); err != nil {
